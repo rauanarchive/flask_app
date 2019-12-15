26a5b1a3f8a3c11ba7f9286dbd278f81fa7ba20a
ra312 <ra312@imperial.ac.uk>
2019-12-15 11:38:02 +0000

    different text

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ rauan_url = Flask(__name__)
 
 @rauan_url.route("/")
 def hello():
-    return "Hello World!"
+    lines = [
+            "My name is Rauan Akylzhanov",
+            "I created this website to train my skills"
+            ]
+    return "\n".join(lines)
+    
 
 if __name__ == "__main__":
     rauan_url.run()
