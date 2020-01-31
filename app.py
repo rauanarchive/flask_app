17cfb4106fc243614f51543578f4c84cac2b81c9
ra312 <akylzhanov.r@gmail.com>
2020-01-31 09:28:47 +0000

    latest changes

--- app.py
+++ app.py
@@ -5,10 +5,39 @@ import pandas as pd
 rauan_url = Flask(__name__,
            template_folder="templates")
 
+import requests
+from twilio.twiml.messaging_response import MessagingResponse
+
+
+@rauan_url.route('/bot', methods=['POST','GET'])
+def bot():
+    incoming_msg = request.values.get('Body', '').lower()
+    resp = MessagingResponse()
+    msg = resp.message()
+    responded = False
+    if 'quote' in incoming_msg:
+        # return a quote
+        r = requests.get('https://api.quotable.io/random')
+        if r.status_code == 200:
+            data = r.json()
+        else:
+            quote = 'I could not retrieve a quote at this time, sorry.'
+        msg.body(quote)
+        responded = True
+    if 'cat' in incoming_msg:
+        # return a cat pic
+        msg.media('https://cataas.com/cat')
+        responded = True
+    if not responded:
+        msg.body('I only know about famous quotes and cats, sorry!')
+    return str(resp)
 
 @rauan_url.route('/cv')
 def download_cv():
-    return url_for('static',filename='Rauan_Akylzhanov_CV.pdf')
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return url_for('static',filename='Rauan_Akylzhanov_CV.pdf')
 @rauan_url.route('/about')
 def about():
     if not session.get('logged_in'):
